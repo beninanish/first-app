09a8991a2f0f38d9ed78bc4f6fd622005e869525
ITLAB <ITLAB@IT208>
2017-01-09 15:49:38 +0530

    Second Commit.

---
+++ src/com/example/test/SecActivity.java
@@ -0,0 +1,26 @@
+package com.example.test;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SecActivity extends Activity {
+	 TextView result;
+	
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_sec);
+		result = (TextView)findViewById(R.id.textView1);
+		result.setText("Test");
+		Typeface custom_font = Typeface.createFromAsset(getAssets(), "Pacifico.ttf");	
+		Intent i = getIntent();
+		String data_from_main_activity = i.getStringExtra("username");
+		result.setTypeface(custom_font);
+		result.setText( data_from_main_activity);
+		
+	}
+
+}
